2a1ec6b065c5f28a9c0ea3d225a54bd1cb244b84
tutujiaw <tujiaw@163.com>
2015-07-26 23:01:20 +0800

    joke

---
+++ QueryPhoneNumber/JokeViewController.swift
@@ -0,0 +1,106 @@
+//
+//  JokeViewController.swift
+//  QueryPhoneNumber
+//
+//  Created by tutujiaw on 15/7/26.
+//  Copyright (c) 2015年 tujiaw. All rights reserved.
+//
+
+import UIKit
+
+class JokeViewController : UIViewController, UITableViewDelegate, UITableViewDataSource,
+UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.estimatedRowHeight = 100
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        self.requestJoke(1)
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 4
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        var joke = ResponseManager.instance.joke
+        if component == 3 {
+            return (joke != nil) ? joke!.allPages : 1
+        } else {
+            return 1
+        }
+    }
+    
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        var joke = ResponseManager.instance.joke
+        if component == 0 {
+            return "记录数"
+        } else if component == 1 {
+            return String(joke?.allNum ?? 0)
+        } else if component == 2 {
+            return "页数"
+        } else if component == 3 {
+            return String(joke != nil ? row + 1 : 0)
+        } else {
+            return ""
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == 3 {
+            self.requestJoke(row)
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let joke = ResponseManager.instance.joke {
+            return joke.contentList.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let jokeCellId = "jokeCellId"
+        let cell = tableView.dequeueReusableCellWithIdentifier(jokeCellId, forIndexPath: indexPath) as! UITableViewCell
+        if let joke = ResponseManager.instance.joke {
+            cell.textLabel?.numberOfLines = 0
+            cell.textLabel?.preferredMaxLayoutWidth = CGRectGetWidth(tableView.bounds)
+            var srcText = joke.contentList[indexPath.row].text
+            var attrStr = NSAttributedString(data: srcText.dataUsingEncoding(NSUnicodeStringEncoding, allowLossyConversion: true)!, options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType], documentAttributes: nil, error: nil)
+            cell.textLabel?.attributedText = attrStr
+        } else {
+            cell.textLabel?.text = ""
+        }
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func requestJoke(page: Int) -> Void {
+        var request = JokeRequest(serverId: "341-1", appId: "4150", time: "yyyy-MM-dd".date(), page: page)
+        println(request.url())
+        Alamofire.manager.request(Method.GET, request.url()).responseJSON(options: NSJSONReadingOptions.MutableContainers, completionHandler: {
+            (_, _, data, err) -> Void in
+            if let data: AnyObject = data {
+                ResponseManager.instance.joke = JokeResponse(data: data)
+                self.tableView.reloadData()
+                self.pickerView.reloadAllComponents()
+            }
+        })
+    }
+}
--- QueryPhoneNumber/ShowApiModel.swift
+++ QueryPhoneNumber/ShowApiModel.swift
@@ -22,13 +22,19 @@ extension String {
         result.destroy()
         return String(format: hash as String)
     }
-}
-
-func getCurrentTime() -> String {
-    var curDate = NSDate()
-    var timeFormatter = NSDateFormatter()
-    timeFormatter.dateFormat = "yyyyMMddHHmmss"
-    return timeFormatter.stringFromDate(curDate)
+    
+    func date() -> String {
+        var curDate = NSDate()
+        var timeFormatter = NSDateFormatter()
+        timeFormatter.dateFormat = self // "yyyyMMddHHmmss"
+        return timeFormatter.stringFromDate(curDate)
+    }
+    
+    func date(date: NSDate) -> String {
+        var timeFormatter = NSDateFormatter()
+        timeFormatter.dateFormat = self
+        return timeFormatter.stringFromDate(date)
+    }
 }
 
 class ShowApiRequest {
@@ -41,11 +47,21 @@ class ShowApiRequest {
     init(serverId: String, appId: String) {
         self.serverId = serverId
         self.appId = appId
-        self.timestamp = getCurrentTime()
+        self.timestamp = "yyyyMMddHHmmss".date()
     }
     
     func systemUrl() -> String {
         return "\(self.route)\(self.serverId)?showapi_appid=\(self.appId)&showapi_timestamp=\(self.timestamp)"
+    }
+}
+
+class ShowApiResponse {
+    var resCode: Int?
+    var resError = ""
+    
+    init(resCode: Int, resError: String) {
+        self.resCode = resCode
+        self.resError = resError
     }
 }
 
@@ -61,4 +77,79 @@ class PhoneNumberRequest : ShowApiRequest {
         var sign = "num\(self.num)showapi_appid\(self.appId)showapi_timestamp\(self.timestamp)\(self.secret)".md5()
         return "\(self.systemUrl())&num=\(self.num)&showapi_sign=\(sign)"
     }
+}
+
+class JokeRequest : ShowApiRequest {
+    var time = ""
+    var page = 1
+    
+    init(serverId: String, appId: String, time: String, page: Int) {
+        super.init(serverId: serverId, appId: appId)
+        self.time = time
+        self.page = page
+    }
+    
+    func url() -> String {
+        var sign = "page\(self.page)showapi_appid\(self.appId)showapi_timestamp\(self.timestamp)time\(self.time)\(self.secret)".md5()
+        return "\(self.systemUrl())&time=\(self.time)&page=\(self.page)&showapi_sign=\(sign)"
+    }
+}
+
+class JokeResponse : ShowApiResponse {
+    struct Content {
+        var tile = ""
+        var text = ""
+        var ct = ""
+    }
+    
+    var allNum: Int = 0
+    var allPages: Int = 0
+    var contentList: [Content] = []
+    var currentPage: Int = 0
+    var maxResult: Int = 0
+    
+    init(data: AnyObject) {
+        var json = JSON(data)
+        var resCode = json["showapi_res_code"].int ?? -1
+        var resError = json["showapi_res_error"].string ?? "response error"
+        super.init(resCode: resCode, resError: resError)
+        
+        if self.resCode == 0 {
+            var body:JSON = json["showapi_res_body"]
+            println(body)
+            if let allNum = body["allNum"].int {
+                self.allNum = allNum
+            }
+            if let allPages = body["allPages"].int {
+                self.allPages = allPages
+            }
+            if let currentPage = body["currentPage"].int {
+                self.currentPage = currentPage
+            }
+            if let maxResult = body["maxResult"].int {
+                self.maxResult = maxResult
+            }
+            if let contentList = body["contentlist"].array {
+                for item in contentList {
+                    var title = item["title"].string
+                    var text = item["text"].string
+                    var ct = item["ct"].string
+                    if title != nil && text != nil && ct != nil {
+                        self.contentList.append(Content(tile: title!, text: text!, ct: ct!))
+                    }
+                }
+            }
+        } else {
+            println(self.resError)
+        }
+    }
+}
+
+class ResponseManager {
+    static var s_instance = ResponseManager()
+    class var instance: ResponseManager {
+        return s_instance
+    }
+    
+    var joke: JokeResponse?
 }
